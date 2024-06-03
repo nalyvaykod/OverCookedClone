083b7660031a97575e33332ffd82289ab8cf8c3b
ranebof <165827905+nalyvaykod@users.noreply.github.com>
2024-06-03 17:33:12 +0300

    Added Player Controller with Rotations

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField] private float Speed = 10f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+
+    private void Update()
+    {
+        //Input 
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+
+        }
+
+        //Move Character
+        inputVector = inputVector.normalized;
+
+        Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        transform.position += movementDirection * Speed * Time.deltaTime;
+
+        //Character Rotation
+        transform.forward = Vector3.Slerp(transform.forward,movementDirection, Time.deltaTime * rotationSpeed);
+        
+
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+}
