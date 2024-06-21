a2848df8ebf2b5cd317bb57ad4816ccf5b5e9363
ranebof <165827905+nalyvaykod@users.noreply.github.com>
2024-06-21 21:00:29 +0300

    Counter Interact

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -7,12 +7,57 @@ public class Player : MonoBehaviour {
     [SerializeField] private float Speed = 10f;
     [SerializeField] private float rotationSpeed = 10f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
+    private Vector3 InteractDirection;
 
     private void Update()
     {
+        HandleMovement();
+        HandleInteraction();
+    }
 
+    //Animation
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteraction()
+    {
+
+        Vector2 inputVector = gameInput.GetMovementVectorNorm();
+        Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if(movementDirection != Vector3.zero)
+        {
+            InteractDirection = movementDirection;
+        }
+
+        //Distance to interact
+        float interactDistance = 2f;
+
+        //Last point of raycast
+        RaycastHit hit;
+
+        //if Raycast fucntion find something,than debug log some text
+        if(Physics.Raycast(transform.position, InteractDirection, out hit, interactDistance, counterLayerMask))
+        {
+            //try to take ClearCounter type of Component
+            if(hit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                //ClearCounter is
+
+                //If true,than call function Interact
+                clearCounter.Interact();
+            }
+        } 
+    }
+
+    //Counters
+    private void HandleMovement()
+    {
         Vector2 inputVector = gameInput.GetMovementVectorNorm();
         Vector3 movementDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
@@ -21,7 +66,7 @@ public class Player : MonoBehaviour {
         float moveDist = Speed * Time.deltaTime; //Дистанція(Швидкість + час)
 
         //Collision Detection
-        bool canMove = !Physics.CapsuleCast(transform.position,transform.position + Vector3.up * playerHeight,playerRadius,movementDirection, moveDist);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, movementDirection, moveDist);
 
         if (!canMove)
         {
@@ -58,23 +103,8 @@ public class Player : MonoBehaviour {
 
 
         //Character Rotation
-        isWalking = movementDirection != Vector3.zero; 
-        transform.forward = Vector3.Slerp(transform.forward,movementDirection, Time.deltaTime * rotationSpeed);
-        
-
-
-    
-    
-    
-    
-    
-    
-    }
-
-    //Animation
-    public bool IsWalking()
-    {
-        return isWalking;
+        isWalking = movementDirection != Vector3.zero;
+        transform.forward = Vector3.Slerp(transform.forward, movementDirection, Time.deltaTime * rotationSpeed);
     }
 
 
